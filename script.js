2d3c26b2073a9523206a28f75ef04d7219d29db1
NIshma k <anukthanish@gmail.com>
2024-09-06 16:00:28 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function checkOddEven(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+
+checkOddEven(10);
+checkOddEven(7);
+
